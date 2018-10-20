32bd402e9369bbf7a9c2692aad037fbd6c61d9ee
samzhab <samzhab@gmail.com>
2018-10-20 21:23:43 +0200

    edit regex for site extraction | add conditional return

--- get_ip_info.rb
+++ get_ip_info.rb
@@ -8,15 +8,18 @@ class GetIpInfo
   def start(site)
     query = setup_ip_query(clean(site))
     response = get_request(query) # max of 150 requests per minute
-    process_json_response(response.body)
+    json_response = process_json_response(response.body)
+    return json_response if json_response['status'] == 'success'
   end
 
   def clean(site)
-    site[/((www)?\.?\w+\.\w+)/]
+    site = site[/^https?((www|.+)?\.?\w+\.[a-zA-Z]+)\/\b/].split("//")
+    site[1] = site[1].to_s.gsub("/","")
+    site
   end
 
   def setup_ip_query(url)
-    "http://ip-api.com/json/#{url}"
+    "http://ip-api.com/json/#{url[1]}"
   end
 
   def process_json_response(response)
